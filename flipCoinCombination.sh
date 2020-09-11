f49d24b32968b77a9eaaa66795eff257acbc93e2
Santhosh V <santhosh.v6426@gmail.com>
2020-09-11 12:44:41 +0530

    Added:Added the program to store the Triplet combinations to dictionary.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,9 @@
 echo "Welcome to Flip Coin Simulation Program"
 read -p "Enter the number of times coin has to be flipped :" n
-echo -e "Select the combination\nEnter 1 for Singlet\nEnter 2 for Doublet"
+echo -e "Select the combination\nEnter 1 for Singlet\nEnter 2 for Doublet\nEnter 3 for triplet"
 read -p "Enter your choice : " comb
 declare -A dict
-arr=('h' 't' 'hh' 'tt' 'ht' 'th')
+arr=('h' 't' 'hh' 'tt' 'ht' 'th' 'hhh' 'ttt' 'htt' 'thh' 'hth' 'tht' 'hht' 'tth' )
 for j in ${arr[@]}
 do
 	dict[$j]=0
@@ -18,21 +18,46 @@ fun() {
 	elif [[ $1 -eq 2 ]]
 	then
                 echo "2"
+        elif [[ $1 -eq 3 ]]
+        then
+                echo "3"
+        elif [[ $1 -eq 4 ]]
+        then
+                echo "4"
+        elif [[ $1 -eq 5 ]]
+        then
+                echo "5"
+        elif [[ $1 -eq 6 ]]
+        then
+                echo "6"
 	else
-		echo "3"
+		echo "7"
 	fi
 }
 for (( i=1; i<=$n; i++ ))
 do
-	a=$((RANDOM%4))
+	case $comb in
+		"1")a=$((RANDOM%2)) ;;
+		"2")a=$((RANDOM%4)) ;;
+		"3")a=$((RANDOM%8)) ;;
+	esac
 	val=$( fun $a )
 	case $val in
 		"0") dict[h]=$(( ${dict[h]} + 1 ))
-			dict[hh]=$(( ${dict[hh]} + 1 )) ;;
+			dict[hh]=$(( ${dict[hh]} + 1 ))
+			dict[hhh]=$(( ${dict[hhh]} +1 )) ;;
 		"1") dict[t]=$(( ${dict[t]} + 1 )) 
-			dict[tt]=$(( ${dict[tt]} + 1 )) ;;
-		"2") dict[ht]=$(( ${dict[ht]} + 1 )) ;;
+			dict[tt]=$(( ${dict[tt]} + 1 )) 
+			dict[ttt]=$(( ${dict[ttt]} + 1 ));;
+		"2") dict[ht]=$(( ${dict[ht]} + 1 ))
+			dict[htt]=$(( ${dict[htt]} + 1 )) ;;
                 "3") dict[th]=$(( ${dict[th]} + 1 ))
+			dict[thh]=$(( ${dict[thh]} + 1 )) ;;
+                "4") dict[hth]=$(( ${dict[hth]} + 1 )) ;;
+                "5") dict[tht]=$(( ${dict[tht]} + 1 )) ;;
+                "6") dict[hht]=$(( ${dict[hht]} + 1 )) ;;
+                "7") dict[tth]=$(( ${dict[tth]} + 1 )) ;;
+
         esac
 done
 if [[ $comb -eq 1 ]]
@@ -41,10 +66,22 @@ then
 	y=`awk 'BEGIN{printf("%0.2f", '${dict[t]}' / '$n' * '100' )}'`
 	echo "Head = $x%"
 	echo "Tail = $y%"
-else
+elif [[ $comb -eq 2 ]]
+then
 	w=`awk 'BEGIN{printf("%0.2f", '${dict[hh]}' / '$n' * '100' )}'`
         x=`awk 'BEGIN{printf("%0.2f", '${dict[tt]}' / '$n' * '100' )}'`
 	y=`awk 'BEGIN{printf("%0.2f", '${dict[ht]}' / '$n' * '100' )}'`
         z=`awk 'BEGIN{printf("%0.2f", '${dict[th]}' / '$n' * '100' )}'`
 	echo -e "HH=$w%\nTT=$x%\nHT=$y%\nTH=$z%"
+else
+        w=`awk 'BEGIN{printf("%0.2f", '${dict[hhh]}' / '$n' * '100' )}'`
+        x=`awk 'BEGIN{printf("%0.2f", '${dict[ttt]}' / '$n' * '100' )}'`
+        y=`awk 'BEGIN{printf("%0.2f", '${dict[htt]}' / '$n' * '100' )}'`
+        z=`awk 'BEGIN{printf("%0.2f", '${dict[thh]}' / '$n' * '100' )}'`
+        w1=`awk 'BEGIN{printf("%0.2f", '${dict[hth]}' / '$n' * '100' )}'`
+        x1=`awk 'BEGIN{printf("%0.2f", '${dict[tht]}' / '$n' * '100' )}'`
+        y1=`awk 'BEGIN{printf("%0.2f", '${dict[hht]}' / '$n' * '100' )}'`
+        z1=`awk 'BEGIN{printf("%0.2f", '${dict[tth]}' / '$n' * '100' )}'`
+	echo -e "HHH=$w%\nTTT=$x%\nHTT=$y%\nTHH=$z%\nHTH=$w1%\nTHT=$x1%\nHHT=$y1%\nTTH=$z1%"
+
 fi
